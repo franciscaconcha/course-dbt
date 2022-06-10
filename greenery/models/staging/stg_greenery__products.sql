a652deeffab352ecebf77a0606f1eadd9f1c112c
Francisca Concha-Ramírez <fran.concha.r@gmail.com>
2022-06-10 16:05:24 +0000

    Add products staging model

---
+++ greenery/models/staging/stg_greenery__products.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized = 'view'
+        , unique_key = 'porduct_guid'
+    )
+}}
+
+WITH products_source AS (
+    SELECT * FROM {{ source('sources', 'products') }}
+)
+
+, rename_recast AS (
+    SELECT
+        product_id AS product_guid
+        , name AS product_name
+        , price AS product_price_usd
+        , inventory AS product_inventory
+    FROM products_source
+)
+
+SELECT * from rename_recast
