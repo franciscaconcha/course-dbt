7cbd2e88cadc9adeea8cb5dc24adb5b3de7f6c9b
Francisca Concha-Ramírez <fran.concha.r@gmail.com>
2022-06-10 16:29:37 +0000

    Fix field names

--- greenery/models/staging/stg_greenery__users.sql
+++ greenery/models/staging/stg_greenery__users.sql
@@ -16,8 +16,8 @@ WITH users_source AS (
         , last_name
         , email
         , phone_number
-        , created_at_utc
-        , updated_at_utc
+        , created_at AS created_at_utc
+        , updates_at AS updated_at_utc
         , address_id AS address_guid
     FROM users_source
 )
